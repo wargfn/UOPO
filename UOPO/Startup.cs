baebb1ce058823fa8eb5a0072a5c8b0ae8603d9e
Ron Schachtner <ron.schachtner@bankofamerica.com>
2018-08-07 05:29:22 -0400

    Add project files.

---
+++ UOPO/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UOPO.Startup))]
+namespace UOPO
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
